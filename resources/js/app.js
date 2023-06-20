f181f5485a32665c8d6697c8648ae6a6c2990b17
Marcelly <souzaguimaraesmarcelly@gmail.com>
2023-06-20 10:08:56 -0300

    Adicionando vue no projeto

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
+import { createApp } from 'vue';
+import Example from './components/Example.vue';
 import './bootstrap';
+
+const app = createApp({
+    components: {
+        Example
+    }
+})
+
+app.mount("#app");
---
+++ resources/js/components/Example.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+   <h1>Exemplo!!!!</h1>
+</template>
